54ce9b5fdd13027ec57db33a3991fc5c170b4bfc
Remi Gau <remi_gau@hotmail.com>
2023-05-18 11:15:29 -0400

    add test smart file path

--- tests/test_smartFilePath.py
+++ tests/test_smartFilePath.py
@@ -12,13 +12,16 @@ def test_path():
     return Path(__file__).parent / "test_data"
 
 
-def test_SmartDirectoryPath(test_path):
+@pytest.mark.parametrize("is_last", [False, True])
+def test_SmartDirectoryPath(test_path, is_last):
     dir_path = SmartFilePath(
         path=test_path / "filetree.tree",
-        parent_smart_path=SmartDirectoryPath(test_path, parent_smart_path=None, is_last=False),
-        is_last=False,
+        parent_smart_path=SmartDirectoryPath(test_path, parent_smart_path=None, is_last=is_last),
+        is_last=is_last,
     )
     assert dir_path.display() == "filetree.tree                                            \n"
-    assert (
-        dir_path.displayable() == "├── filetree.tree                                            \n"
-    )
+
+    expected = "├── filetree.tree                                            \n"
+    if is_last:
+        expected = "└── filetree.tree                                            \n"
+    assert dir_path.displayable() == expected
